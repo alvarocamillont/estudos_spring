8d5943f8b183c6ddc304aa77cfb18bf3fc05e793
Alvaro <alvaro.camilloneto@gmail.com>
2021-09-26 16:17:12 -0300

    Inicio do projeto

---
+++ coffee_book/src/test/java/com/example/coffee_book/CoffeeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffee_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
